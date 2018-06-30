9f6583d7e1960d546cf9042200dc45399d3a4f9c
Jigar Mehta <jigar@evolvingweb.ca>
2018-06-30 14:31:03 -0400

    Update plugin information

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :toggl2redmine do
   name 'Toggl 2 Redmine'
   author 'Jigar Mehta'
   description 'Imports time entries from Toggl into Redmine'
-  version '0.0.5'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  version '0.9.0'
+  url 'https://github.com/evolvingweb/toggl2redmine'
+  author_url 'https://www.linkedin.com/in/jigarius'
 end

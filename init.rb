65d646b27a6d4b99285b5fadfdce708489f28448
Jigar Mehta <jigar@evolvingweb.ca>
2018-07-11 10:56:27 -0400

    Add a menu item in the application menu; fixes #16

--- init.rb
+++ init.rb
@@ -1,8 +1,12 @@
 Redmine::Plugin.register :toggl2redmine do
+  # Package info.
   name 'Toggl 2 Redmine'
   author 'Jigar Mehta'
   description 'Imports time entries from Toggl into Redmine'
   version '0.9.0'
   url 'https://github.com/evolvingweb/toggl2redmine'
   author_url 'https://www.linkedin.com/in/jigarius'
+
+  # Menu items.
+  menu :application_menu, :toggl2redmine, { :controller => 't2r', :action => 'index' }, :caption => 'Toggl 2 Redmine'
 end

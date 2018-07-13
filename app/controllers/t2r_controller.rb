8dbcd0497c40be533742fdb6fc27596a02af7a2a
Jigar Mehta <jigar@evolvingweb.ca>
2018-07-13 16:45:57 -0400

    Associate controller to the menu item
    This marks the menu item as active when a user visits it.

--- app/controllers/t2r_controller.rb
+++ app/controllers/t2r_controller.rb
@@ -1,5 +1,7 @@
 class T2rController < ApplicationController
 
+  menu_item :toggl2redmine
+
   def index
     @toggl_api_key = User.current.custom_field_value(UserCustomField.find_by_name('Toggl API Key'))
     @redmine_api_key = User.current.api_key

4f340812029e3c148167a972ef70a91ca92cec25
Jigar Mehta <jigar@evolvingweb.ca>
2018-07-17 19:59:50 -0400

    Make toggl_time_entry.toggl_id unique; refs #13

--- db/migrate/002_create_toggl_time_entries.rb
+++ db/migrate/002_create_toggl_time_entries.rb
@@ -2,7 +2,7 @@ class CreateTogglTimeEntries < ActiveRecord::Migration
 
   def change
     create_table :toggl_time_entries do |t|
-      t.integer :toggl_id, null: false, index: true
+      t.integer :toggl_id, null: false, index: true, unique: true
       t.belongs_to :time_entry, null: false
       t.datetime :created_at, null: false
     end

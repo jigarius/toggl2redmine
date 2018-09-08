0d45579bb826a7062f027bbfd58abf16fa3584b9
Jigar Mehta <jigarius@gmail.com>
2018-09-08 12:48:20 -0400

    Update comment

--- app/controllers/t2r_controller.rb
+++ app/controllers/t2r_controller.rb
@@ -3,13 +3,15 @@ class T2rController < ApplicationController
   menu_item :toggl2redmine
   before_action :require_login, :validate_user
 
-  # TODO: Check for user permissions.
+  # TODO: Check if user is allowed to log time.
 
+  # Provides an interface for importing Toggl time entries to Redmine.
   def index
     @toggl_api_key = @user.custom_field_value(UserCustomField.find_by_name('Toggl API Key'))
     @redmine_api_key = @user.api_key
   end
 
+  # Creates time entries from request data.
   def import
     # Prepare a Redmine time entry.
     @time_entry = TimeEntry.new(params[:time_entry])
@@ -69,6 +71,7 @@ class T2rController < ApplicationController
     render :json => { :time_entry => @time_entry }, :status => 201
   end
 
+  # Determines the currently logged in user.
   def validate_user
     @user = User.current
 

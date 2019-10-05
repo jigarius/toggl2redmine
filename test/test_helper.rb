43b2148889f78c6aacf264600773f697f4cf997d
Jigar Mehta <jigarius@gmail.com>
2019-10-05 13:30:49 -0400

    refs #52: Automated plugin tests with Travis.

---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+# Load the normal Rails helper
+require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
---
+++ test/unit/t2r_unit_test.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require File.dirname(__FILE__) + '/../test_helper'
+
+class T2rUnitTest < ActiveSupport::TestCase
+  test '1=1' do
+    assert_equal 1, 1
+  end
+end

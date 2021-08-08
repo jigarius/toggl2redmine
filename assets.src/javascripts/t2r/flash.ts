8742f18c1f9d25f4269efcc69d32714d26d37823
Jigar Mehta <hello@jigarius.com>
2021-08-08 17:28:08 -0400

    Remove any types from flash.ts

--- assets.src/javascripts/t2r/flash.ts
+++ assets.src/javascripts/t2r/flash.ts
@@ -7,25 +7,25 @@ enum Type {
 /**
  * Displays a rails-style flash message.
  */
-function message(text: string, type: Type = Type.Notice) {
+function message(text: string, type: Type = Type.Notice): void {
   $('#content').prepend(`<div class="flash t2r ${type}">${text.trim()}</div>`);
 }
 
-export function error(text: string) {
+export function error(text: string): void {
   message(text, Type.Error)
 }
 
-export function warning(text: string) {
+export function warning(text: string): void {
   message(text, Type.Warning)
 }
 
-export function notice(text: string) {
+export function notice(text: string): void {
   message(text, Type.Notice)
 }
 
 /**
  * Clears all flash messages.
  */
-export function clear() {
+export function clear(): void {
   $('.t2r.flash').remove();
 }

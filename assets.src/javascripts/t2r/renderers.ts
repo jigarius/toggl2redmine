9852851a45b0bd67d815373983a7ecdcf027aaba
Jigar Mehta <hello@jigarius.com>
2021-08-28 20:41:20 -0400

    Remove any type usage from renderers

--- assets.src/javascripts/t2r/renderers.ts
+++ assets.src/javascripts/t2r/renderers.ts
@@ -1,26 +1,26 @@
-import * as utils from "./utils.js";
-import * as datetime from "./datetime.js";
+import * as models from './models.js'
+import * as utils from './utils.js'
+import * as datetime from './datetime.js'
 
-declare const T2R_BUTTON_ACTIONS: string;
-declare const contextMenuRightClick: any;
+declare const T2R_BUTTON_ACTIONS: string
+declare const contextMenuRightClick: { (): void }
 
-export function renderRedmineProjectLabel(project: any): string {
-  project ||= { name: 'Unknown', path: 'javascript:void(0)', status: 1 };
-  project.classes = ['project'];
+export function renderRedmineProjectLabel(project: models.Project): string {
+  const classes = ['project'];
   if (project.status != 1) {
-    project.classes.push('closed');
+    classes.push('closed')
   }
 
-  return '<a href="' + project.path + '" class="' + project.classes.join(' ') + '"><strong>'
+  return '<a href="' + project.path + '" class="' + classes.join(' ') + '"><strong>'
     + utils.htmlEntityEncode(project.name)
     + '</strong></a>';
 }
 
-export function renderRedmineIssueLabel(issue: any): string {
-  if (typeof issue['id'] === 'undefined' || !issue.id) return '-'
-  if (!issue.subject) return issue.id.toString()
+export function renderRedmineProjectStubLabel(): string {
+  return '<span class="project unknown">-</span>'
+}
 
-  // Render a clickable issue label.
+export function renderRedmineIssueLabel(issue: models.Issue): string {
   return '<a href="' + issue.path + '" target="_blank">'
     + utils.htmlEntityEncode(issue ? issue.tracker.name : '-')
     + utils.htmlEntityEncode(issue ? ' #' + issue.id : '')
@@ -28,22 +28,34 @@ export function renderRedmineIssueLabel(issue: any): string {
     + '</a>'
 }
 
-export function renderTogglRow(data: any): JQuery<HTMLElement> {
+export function renderRedmineIssueStubLabel(issueId: number | null): string {
+  if (!issueId) {
+    return 'Unknown'
+  }
+
+  return '#' + issueId.toString() + ': -'
+}
+
+export function renderTogglRow(data: models.TogglTimeEntry): JQuery<HTMLElement> {
   const issue = data.issue
-  const issueLabel = renderRedmineIssueLabel(issue || { id: data.id })
-  const project = data.project || null;
-  const projectLabel = renderRedmineProjectLabel(project)
-  const oDuration = data.duration;
-  const rDuration = data.roundedDuration;
+  const issueLabel: string = issue ? renderRedmineIssueLabel(issue) : renderRedmineIssueStubLabel(data.issue_id)
+  const project = data.project || null
+  const projectLabel = project ? renderRedmineProjectLabel(project) : renderRedmineProjectStubLabel()
+  const oDuration = data.duration as datetime.Duration
+  const rDuration = data.roundedDuration as datetime.Duration
 
   const markup = '<tr data-t2r-widget="TogglRow">'
     + '<td class="checkbox"><input class="cb-import" type="checkbox" value="1" title="Check this box if you want to import this entry." /></td>'
     + '<td class="status"></td>'
     + '<td class="issue">'
-    + '<input data-property="issue_id" type="hidden" data-value="' + utils.htmlEntityEncode(issue ? issue.id : '') + '" value="' + (issue ? issue.id : '') + '" />'
+    + '<input data-property="issue_id" type="hidden" '
+    + 'data-value="' + utils.htmlEntityEncode(issue ? issue.id.toString() : '') + '" '
+    + 'value="' + utils.htmlEntityEncode(issue ? issue.id.toString() : '') + '" />'
     + projectLabel + '<br />' + issueLabel
     + '</td>'
-    + '<td class="comments"><input data-property="comments" type="text" value="' + utils.htmlEntityEncode(data.comments) + '" maxlength="255" /></td>'
+    + '<td class="comments">'
+    + '<input data-property="comments" type="text" value="' + utils.htmlEntityEncode(data.comments) + '" maxlength="255" />'
+    + '</td>'
     + '<td class="activity">'
     + '<select data-property="activity_id" required="required" data-placeholder="-" data-t2r-widget="RedmineActivityDropdown"></select>'
     + '</td>'
@@ -56,9 +68,8 @@ export function renderTogglRow(data: any): JQuery<HTMLElement> {
   const $tr = $(markup);
   $tr.data('t2r.entry', data);
 
-  let statusLabel: any = null;
+  let statusLabel: HTMLElement | null = null
 
-  // Status specific actions.
   switch (data.status) {
     case 'pending':
       if (data.errors.length > 0) {
@@ -96,13 +107,13 @@ export function renderTogglRow(data: any): JQuery<HTMLElement> {
   return $tr;
 }
 
-export function renderRedmineRow(data: any): JQuery<HTMLElement> {
+export function renderRedmineRow(data: models.TimeEntry): JQuery<HTMLElement> {
   const issue = data.issue
   const issueLabel = renderRedmineIssueLabel(issue)
   const project = data.project
   const projectLabel = renderRedmineProjectLabel(project)
-  const dur = new datetime.Duration(data.duration)
-  dur.roundTo(1, datetime.RoundingMethod.Up)
+  const oDuration = data.duration
+  oDuration.roundTo(1, datetime.RoundingMethod.Up)
 
   const markup = '<tr id="time-entry-' + data.id + '"  class="time-entry hascontextmenu">'
     + '<td class="subject">'
@@ -111,7 +122,7 @@ export function renderRedmineRow(data: any): JQuery<HTMLElement> {
     + '</td>'
     + '<td class="comments">' + utils.htmlEntityEncode(data.comments) + '</td>'
     + '<td class="activity">' + utils.htmlEntityEncode(data.activity.name) + '</td>'
-    + '<td class="hours">' + dur.asHHMM() + '</td>'
+    + '<td class="hours">' + oDuration.asHHMM() + '</td>'
     + '<td class="buttons">' + T2R_BUTTON_ACTIONS + '</td>'
     + '</tr>';
 
@@ -142,6 +153,7 @@ export function renderImportStatusLabel(
   const el = document.createElement('span')
 
   el.innerHTML = label
+  el.dataset.t2rWidget = 'Tooltip'
   el.classList.add('icon', `icon-${icon}`)
   if (description) {
     el.setAttribute('title', description)

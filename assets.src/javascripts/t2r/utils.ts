54e616c0404fa5d7e7587a0db197866d88b9b816
Jigar Mehta <hello@jigarius.com>
2021-09-03 17:36:46 -0400

    Create EventManager for handling custom events

--- assets.src/javascripts/t2r/utils.ts
+++ assets.src/javascripts/t2r/utils.ts
@@ -27,3 +27,39 @@ export function getDateFromLocationHash(): string | undefined {
     console.debug('Date not detected in URL fragment')
   }
 }
+
+/**
+ * Event Callback.
+ *
+ * Callbacks for custom events.
+ */
+export interface EventListener { (): void }
+
+/**
+ * Event Manager.
+ *
+ * Handles registry and dispatch of events.
+ */
+export class EventManager {
+  readonly listeners: { [index: string]: EventListener[] }
+
+  constructor() {
+    this.listeners = {}
+  }
+
+  public on(eventName: string, callback: EventListener): void {
+    if (typeof this.listeners[eventName] === 'undefined') {
+      this.listeners[eventName] = []
+    }
+
+    this.listeners[eventName].push(callback)
+  }
+
+  public trigger<Type>(eventName: string): void {
+    if (typeof this.listeners[eventName] === 'undefined') return
+
+    for (const listener of this.listeners[eventName]) {
+      const result = listener()
+    }
+  }
+}

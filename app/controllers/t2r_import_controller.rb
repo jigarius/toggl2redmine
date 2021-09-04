cc103b8dff2dd88041a0d35edfe692baa60fe99a
Jigar Mehta <hello@jigarius.com>
2021-09-04 10:34:16 -0400

    Make UI messages translatable

--- app/controllers/t2r_import_controller.rb
+++ app/controllers/t2r_import_controller.rb
@@ -18,7 +18,9 @@ class T2rImportController < T2rBaseController
     @translations = translation_hash(
       't2r.error.ajax_load',
       't2r.error.list_empty',
-      't2r.error.no_entries_selected'
+      't2r.error.no_entries_selected',
+      't2r.error.date_invalid',
+      't2r.import_confirmation'
     )
   end
 
